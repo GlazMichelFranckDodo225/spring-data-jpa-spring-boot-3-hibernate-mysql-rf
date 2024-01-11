54ef889abb4fbc2efe7ae40419ed3394f204aa81
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-11 09:38:53 +0100

    Primary key generation strategies: GenerationType.AUTO

--- src/main/java/com/dmgf/entity/Product.java
+++ src/main/java/com/dmgf/entity/Product.java
@@ -27,7 +27,7 @@ import java.time.LocalDateTime;
         }
 )
 public class Product {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name = "stock_keeping_unit", nullable = false)
     private String sku;
