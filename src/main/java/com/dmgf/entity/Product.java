c75158ea5e50e631f41577187e0f0f10a641c132
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-11 08:52:55 +0100

    Create Product Entity with @Entity and @Id

---
+++ src/main/java/com/dmgf/entity/Product.java
@@ -0,0 +1,25 @@
+package com.dmgf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity
+public class Product {
+    @Id
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+    private LocalDateTime dateCreated;
+    private LocalDateTime lastUpdated;
+}
