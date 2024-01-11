97a1818c3909def4b00dfd87145defb69da3f451
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-11 07:33:56 +0100

    Project Initialization

---
+++ src/test/java/com/dmgf/SpringDataJpaSpringBoot3HibernateMysqlRfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmgf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaSpringBoot3HibernateMysqlRfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
